69ebed5a4763433671dc3f9223a4cec67934e5bf
mbiereth89 <mbiereth89@gmail.com>
2025-05-07 15:46:36 +0000

    contact bg, cleaner grid

--- components/About/Section.vue
+++ components/About/Section.vue
@@ -1,58 +1,55 @@
 <template>
-    <div class="border-b border-muted">
-    <UContainer class="flex flex-col py-24 !px-4">
+
+  <div class="border-b border-muted">
+    <UContainer class="py-24 !px-4">
       <h2 class="max-w-5xl mx-auto text-center">Kreative & attraktive Lösungen für komplexe Herausforderungen</h2>
     </UContainer>
   </div>
 
-    <div class="border-b border-muted">
-      <UContainer class="grid grid-cols-1 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:border-r lg:border-l border-muted">
+  <div class="border-b border-muted">
+    <UContainer class="grid grid-cols-1 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:border-r lg:border-l border-muted">
+      <div class="border-b lg:border-b-0 lg:border-r border-muted py-12 lg:row-span-2">
+        <ClientOnly>
+          <img :src="getImage('about')" alt="Beschreibung des Bildes" class="aspect-square object-cover object-center" />
+        </ClientOnly>
+      </div>
 
-        <div class="border-b lg:border-b-0 lg:border-r border-muted py-12 lg:row-span-2">
-          <div class="flex flex-col items-start gap-4 h-full">
-            <ClientOnly>
-              <img :src="getImage('about')" alt="Beschreibung des Bildes" class="min-w-full min-h-full object-cover object-center" />
-            </ClientOnly>
+      <div class="flex flex-col justify-center border-b border-muted px-8 py-12">
+        <div class="space-y-2">
+          <div v-intersection-observer.once="handleIntersection" class="isolate flex mb-4 size-16 rounded-md bg-muted">
+            <UIcon v-if="isVisible" name="i-line-md-github-loop" class="size-12 mx-auto my-auto" />
+          </div>
+          <h4>Wer bin ich?</h4>
+          <p class="text-lg text-muted mb-6 max-w-lg">
+            Ich gestalte Webseiten und andere digitale Produkte seit ca. 20 Jahren aus Leidenschaft.
+          </p>
+          <div class="flex gap-2">
+            <DrawerCv />
+            <UButton to="https://github.com/louzed" target="_blank" icon="i-simple-icons-github" variant="ghost"
+              color="neutral" size="lg" />
+            <UButton to="https://dribbble.com/mbiereth" target="_blank" icon="i-mingcute-dribbble-fill" variant="ghost"
+              color="neutral" size="lg" />
           </div>
         </div>
+      </div>
 
-        <div class="border-b border-muted px-8 py-16">
-          <div class="flex flex-col items-start gap-2">
-            <div v-intersection-observer.once="handleIntersection"
-              class="isolate flex mb-2 size-16 rounded-md bg-muted overflow-hidden">
-              <UIcon v-if="isVisible" name="i-line-md-github-loop" class="size-12 mx-auto my-auto" />
-            </div>
-            <h4>Wer bin ich?</h4>
-            <p class="text-lg text-muted mb-4 max-w-lg">
-              Ich gestalte Webseiten und andere digitale Produkte seit ca. 20 Jahren aus Leidenschaft. 
-            </p>
-            <div class="flex gap-2">
-              <DrawerCv />            
-              <UButton to="https://github.com/louzed" target="_blank" icon="i-simple-icons-github" variant="ghost" color="neutral" size="lg" />
-              <UButton to="https://dribbble.com/mbiereth" target="_blank" icon="i-mingcute-dribbble-fill" variant="ghost" color="neutral" size="lg" />
-            </div>
+      <div class="px-8 py-12 flex flex-col justify-center">
+        <div class="space-y-2">
+          <div v-intersection-observer.once="handleIntersection2" class="isolate flex size-16 mb-4 rounded-md bg-muted">
+            <UIcon v-if="isVisible2" name="i-line-md-star-pulsating-loop" class="size-12 mx-auto my-auto" />
+          </div>
+          <h4>Certified UX Designer</h4>
+          <p class="text-lg text-muted mb-6 max-w-lg">
+            2022 absolvierte ich eine Weiterbildung zum UX/UI Designer und arbeite seitdem freiberuflich.
+          </p>
+          <div class="flex gap-2">
+            <DrawerXdi />
+            <DrawerWebflow />
           </div>
         </div>
-
-        <div class="px-8 py-16">
-          <div class="flex flex-col items-start gap-2">
-            <div v-intersection-observer.once="handleIntersection2"
-              class="isolate size-16 flex mb-2 rounded-md bg-muted overflow-hidden">
-              <UIcon v-if="isVisible2" name="i-line-md-star-pulsating-loop" class="size-12 mx-auto my-auto" />
-            </div>
-            <h4>Certified UX Designer</h4>
-            <p class="text-lg text-muted mb-4 max-w-lg">
-              2022 absolvierte ich eine Weiterbildung zum UX/UI Designer und arbeite seitdem freiberuflich.
-            </p>
-            <div class="flex gap-2">
-              <DrawerXdi />
-              <DrawerWebflow />
-            </div>
-          </div>
-        </div>
-
-      </UContainer>
-    </div>
+      </div>
+    </UContainer> 
+  </div>
 
   <AboutAccordion />
 </template>
--- components/App/Contact.vue
+++ components/App/Contact.vue
@@ -1,28 +1,27 @@
 <template>
-    <div class="border-b border-muted py-12 lg:py-24">
-      <UContainer
-        class="flex flex-col bg-muted py-16 !px-4 items-center justify-center relative overflow-hidden">
-        <div
-          v-intersection-observer.once="handleIntersection"
-          class="size-16 flex justify-center items-center rounded-md overflow-hidden mb-4"
-        >
-          <UIcon
-            v-if="isVisible"
-            name="i-line-md-coffee-half-empty-filled-loop"
-            class="isolate size-16"
-          />
-        </div>
-        <h3 class="text-center mb-2 text-foreground">Lassen Sie uns sprechen</h3>
-        <p class="text-lg text-muted text-center mb-8">Ich freue mich auf Ihre Nachricht!
-        </p>
-        <div class="flex gap-2">
-          <UButton to="https://wa.me/491785785578?text=Hallo%20Markus,%20" target="blank" label="WhatsApp" size="lg"
-            icon="i-mdi-whatsapp" color="success" />
-          <UButton to="https://zeeg.me/biereth" target="blank" label="Termin buchen" size="lg"
-            icon="i-mdi-external-link" trailing />
-        </div>
-      </UContainer>
-    </div>
+  <div class="border-b border-muted py-12 lg:py-24 relative overflow-hidden">
+    <UContainer class="relative flex flex-col py-16 !px-4">
+      <div class="absolute inset-0 bg-[url('/images/contact-light.jpg')] dark:bg-[url('/images/contact-dark.jpg')] 
+        bg-fixed bg-cover bg-center bg-muted pointer-events-none">
+      </div>
+      
+      <div v-intersection-observer.once="handleIntersection" class="size-16 flex mx-auto mb-4 relative z-10">
+        <UIcon
+          v-if="isVisible"
+          name="i-line-md-coffee-half-empty-filled-loop"
+          class="isolate size-16 mx-auto"
+        />
+      </div>
+      <h3 class="text-center mb-2 relative z-10">Lassen Sie uns sprechen</h3>
+      <p class="text-lg text-muted text-center mb-8 relative z-10">Ich freue mich auf Ihre Nachricht!</p>
+      <div class="flex gap-2 mx-auto relative z-10">
+        <UButton to="https://wa.me/491785785578?text=Hallo%20Markus,%20" target="blank" label="WhatsApp" size="lg"
+          icon="i-mdi-whatsapp" color="success" />
+        <UButton to="https://zeeg.me/biereth" target="blank" label="Termin buchen" size="lg"
+          icon="i-mdi-external-link" trailing />
+      </div>
+    </UContainer>
+  </div>
 </template>
 
 <script setup lang="ts">
